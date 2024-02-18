15837fe5eeba3583be5353e6123b891b479b04e6
michg.grabowski <michg.grabowski@gmail.com>
2024-02-18 13:21:52 +0100

    Third commit

---
+++ Zad 11.py
@@ -0,0 +1,8 @@
+print ("Kalkulatro dodawani liczb całkowitych")
+
+x = int(input("Podaj pierwszą liczbę całkowitą: "))
+y = int(input("Podaj drugą liczbę całkowitą: "))
+
+z= (x+y)
+
+print ("Dodawanie dwóch liczb wynik to = ", z)
